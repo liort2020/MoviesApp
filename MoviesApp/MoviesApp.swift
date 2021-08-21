d3ea1ca3d8f87582664be72e41ff232d9697b20b
liort2020 <liort2020@gmail.com>
2021-08-21 16:03:26 +0300

    first commit

--- MoviesApp/MoviesApp.swift
+++ MoviesApp/MoviesApp.swift
@@ -1,5 +1,5 @@
 //
-//  MoviesAppApp.swift
+//  MoviesApp.swift
 //  MoviesApp
 //
 //  Created by Lior Tal on 21/08/2021.
@@ -8,7 +8,7 @@
 import SwiftUI
 
 @main
-struct MoviesAppApp: App {
+struct MoviesApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
